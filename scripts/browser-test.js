dca51a3414dbbbaff5d5fa2a6ea3ad4a807bb376
Ayakha Ntsomi <132166079+ST10283036@users.noreply.github.com>
2026-09-24 23:32:53 +0200

    Fix merge conflict in browser-test.js
    Resolve merge conflict in browser-test.js

--- scripts/browser-test.js
+++ scripts/browser-test.js
@@ -10,10 +10,10 @@ const path = require('path');
 
 const BASE =
   process.env.PPC_BASE || 'http://localhost:8124';
-<<<<<<< HEAD
+
 const SHOTS = path.join(__dirname, '..', 'browser-shots');
 const SHOTS_ONLY = process.argv.indexOf('--shots-only') !== -1;
-=======
+
 
 const CHROME =
   process.env.PPC_CHROME || undefined;
@@ -23,7 +23,7 @@ const SHOTS =
 
 const SHOTS_ONLY =
   process.argv.indexOf('--shots-only') !== -1;
->>>>>>> upstream/main
+
 
 const PASS = 'PASS', FAIL = 'FAIL', INFO = 'INFO';
 const results = [];
